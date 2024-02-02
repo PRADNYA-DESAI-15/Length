05362a9a0bf4ee2c7f27eb5bb8056a98172834d6
Pradnya Desai <115803674+PRADNYA-DESAI-15@users.noreply.github.com>
2024-02-02 14:01:20 +0530

    Java

---
+++ Length/src/com/DayOne/GeometryMain.java
@@ -0,0 +1,10 @@
+package com.DayOne;
+
+public class GeometryMain {
+	public static void main(String[] args) { CalculateLength cl = new CalculateLength();
+	cl.getLength();
+	cl.findEquality();
+	cl.checkCompareLine();
+	}
+
+}
